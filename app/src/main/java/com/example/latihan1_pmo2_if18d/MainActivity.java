3da13ec9b756b7393be79087893284b949b64de0
NaufalR111 <rasyada321@gmail.com>
2020-11-02 07:56:02 +0700

    first commit

---
+++ app/src/main/java/com/example/latihan1_pmo2_if18d/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.latihan1_pmo2_if18d;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
